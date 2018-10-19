6636fd23ff13aaf38b56ca8de8a54eda51729ec8
Hariraj <raja70a@gmail.com>
2018-10-19 18:48:41 +0530

    Updates

---
+++ precache-manifest.a3bd53d21e5610012dba9a7029693f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "e8d7d3a1845cd4996150",
+    "url": "/myapp/static/js/main.e8d7d3a1.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/myapp/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e8d7d3a1845cd4996150",
+    "url": "/myapp/static/css/main.9f69f3ce.chunk.css"
+  },
+  {
+    "revision": "06b5d4386ee2e2508e1b396ff434b944",
+    "url": "/myapp/index.html"
+  }
+];
